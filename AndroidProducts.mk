5ead950010b81361699f1a0b4cb552f2a66e9282
codebht <bhavitpal1412@gmail.com>
2026-09-15 21:24:03 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,9 +6,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_Infinix-X6711.mk
+    $(LOCAL_DIR)/twrp_Infinix-X6711.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_Infinix-X6711-user \
-    omni_Infinix-X6711-userdebug \
-    omni_Infinix-X6711-eng
+    twrp_Infinix-X6711-eng
