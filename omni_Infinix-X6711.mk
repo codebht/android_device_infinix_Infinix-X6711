31ecd6b5060a231c74f32ad1c03fbf4288a627f8
codebht <bhavitpal1412@gmail.com>
2026-09-15 21:24:25 +0530

    Update omni_Infinix-X6711.mk

--- omni_Infinix-X6711.mk
+++ omni_Infinix-X6711.mk
@@ -5,18 +5,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-
-# Inherit from Infinix-X6711 device
 $(call inherit-product, device/infinix/Infinix-X6711/device.mk)
 
 PRODUCT_DEVICE := Infinix-X6711
-PRODUCT_NAME := omni_Infinix-X6711
+PRODUCT_NAME := twrp_Infinix-X6711
 PRODUCT_BRAND := Infinix
 PRODUCT_MODEL := Infinix X6711
 PRODUCT_MANUFACTURER := infinix
